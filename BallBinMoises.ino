c5dd94cbd534a678539f638a82c0a3d09cd85ce2
carloslavig <carloslavig@gmail.com>
2025-12-04 11:03:37 -0300

    second update

---
+++ BallBinMoises.ino
@@ -0,0 +1,77 @@
+// note: for my beam to be horizontal, Servo Motor angle should be 135 degrees.
+
+#include <Servo.h> 
+
+ 
+
+Servo meuServo; 
+
+int pos;
+
+ 
+
+
+ 
+
+
+#include <Servo.h>
+
+#include <hcsr04.h>
+
+
+#define TRIG 12
+
+#define ECHO 13
+
+
+
+
+
+
+HCSR04 ultrassom(TRIG, ECHO, 40, 400);
+
+
+void setup()
+
+ {
+
+
+
+    meuServo.attach(6); 
+
+    meuServo.write(0);
+
+
+  Serial.begin(9600);
+
+
+
+  Serial.print("setup\n");
+
+}
+
+
+void loop() {
+
+
+pos = 135;
+        meuServo.write(pos); 
+
+        delay(15);
+
+
+
+    delay(10);
+
+
+  int dist_mm = ultrassom.distanceInMillimeters();
+
+  if (dist_mm >= 300) {return;}
+
+  if (dist_mm<40) {dist_mm=40;}
+
+  Serial.println(dist_mm);
+
+  delay(10);
+
+}
